2990dfa93383f81406fa66c4b04f9d9df1560835
SalehAhmed19 <saleh.ahmed.mahin@gmail.com>
2025-10-02 18:40:41 +0600

    logout

--- client/src/components/SideBar.jsx
+++ client/src/components/SideBar.jsx
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useContext } from "react";
 import assets, { userDummyData } from "../assets/assets";
 import { Link } from "react-router-dom";
+import { AuthContext } from "../../context/context";
 
 export default function SideBar({ selectedUser, setSelectedUser }) {
+  const { logout } = useContext(AuthContext);
   return (
     <div
       className={`bg-[#8185b2]/10 h-full p-5 rounded-r-xl overflow-y-scroll text-white ${
@@ -25,7 +27,9 @@ export default function SideBar({ selectedUser, setSelectedUser }) {
                 Edit profile
               </Link>
               <hr className="my-2 border-t border-gray-500" />
-              <p className="cursor-pointer text-sm">Logout</p>
+              <p onClick={() => logout()} className="cursor-pointer text-sm">
+                Logout
+              </p>
             </div>
           </div>
         </div>
